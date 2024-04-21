65a5ddc8e635ba8807a8ed3eb500607912406770
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2024-04-21 21:52:16 +0300

    Admin panelinin entegre edilmesi ve componentlere bölünmesi

---
+++ MyPortfolioProject/Controllers/ExperienceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolioProject.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        public IActionResult ExperienceList()
+        {
+            return View();
+        }
+    }
+}
