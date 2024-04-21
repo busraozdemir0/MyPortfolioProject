a68ab0c4063c89bd2ad1d3dedaef7ef23021a0ee
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2024-04-21 22:55:57 +0300

    Mesaj sayfası ve işlemleri

---
+++ MyPortfolioProject/Controllers/MessageController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolioProject.DAL.Context;
+
+namespace MyPortfolioProject.Controllers
+{
+	public class MessageController : Controller
+	{
+		AppDbContext _context = new AppDbContext();
+		public IActionResult Inbox()
+		{
+			var values = _context.Messages.ToList();
+			return View(values);
+		}
+
+		public IActionResult ChangeIsReadToTrue(int id) // Mesaji okundu olarak isaretlemek icin
+		{
+			var value = _context.Messages.Find(id);
+			value.IsRead = true;
+			_context.SaveChanges();
+			return RedirectToAction("Inbox");
+		}
+
+        public IActionResult ChangeIsReadToFalse(int id) // Mesaji okunmadi olarak isaretlemek icin
+        {
+            var value = _context.Messages.Find(id);
+            value.IsRead = false;
+            _context.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+
+		public IActionResult DeleteMessage(int id)
+		{
+            var value = _context.Messages.Find(id);
+			_context.Messages.Remove(value);
+			_context.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+
+        public IActionResult MessageDetail(int id)
+		{
+            var value = _context.Messages.Find(id);
+			return View(value);
+        }
+
+    }
+}
