10c216d9bb5d5b10bd9b3047916085ff1d1a4fb4
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2024-04-21 22:36:04 +0300

    İstatistikler sayfası

---
+++ MyPortfolioProject/Controllers/StatisticController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolioProject.DAL.Context;
+
+namespace MyPortfolioProject.Controllers
+{
+    public class StatisticController : Controller
+    {
+        AppDbContext _context = new AppDbContext();
+        public IActionResult Index()
+        {
+            ViewBag.skillCount = _context.Skills.Count();
+            ViewBag.messageCount = _context.Messages.Count();
+            ViewBag.falseMessageCount = _context.Messages.Where(x => x.IsRead == false).Count(); // Okunmayan mesaj sayisi
+            ViewBag.trueMessageCount = _context.Messages.Where(x => x.IsRead == true).Count(); // Okunan mesaj sayisi
+            return View();
+        }
+    }
+}
