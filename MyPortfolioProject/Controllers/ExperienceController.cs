63b07a07ca796f0a5fcf7ea5dc6aaee3e5550cf6
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2024-04-21 22:24:51 +0300

    Deneyim ekleme, güncelleme ve silme

--- MyPortfolioProject/Controllers/ExperienceController.cs
+++ MyPortfolioProject/Controllers/ExperienceController.cs
@@ -1,12 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolioProject.DAL.Context;
+using MyPortfolioProject.DAL.Entities;
 
 namespace MyPortfolioProject.Controllers
 {
     public class ExperienceController : Controller
     {
+        AppDbContext _context = new AppDbContext();
         public IActionResult ExperienceList()
+        {
+            var values = _context.Experiences.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult CreateExperience()
         {
             return View();
         }
-    }
+		[HttpPost]
+		public IActionResult CreateExperience(Experience experience)
+		{
+            _context.Experiences.Add(experience);
+            _context.SaveChanges();
+			return RedirectToAction("ExperienceList");
+		}
+		[HttpGet]
+		public IActionResult UpdateExperience(int id)
+		{
+			var value = _context.Experiences.Find(id);
+			return View(value);
+		}
+		[HttpPost]
+		public IActionResult UpdateExperience(Experience experience)
+		{
+            _context.Experiences.Update(experience);
+			_context.SaveChanges();
+			return RedirectToAction("ExperienceList");
+		}
+
+		public IActionResult DeleteExperience(int id)
+        {
+            var value = _context.Experiences.Find(id);
+            _context.Experiences.Remove(value);
+            _context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+	}
 }
